af736301fbad77bd510e4560fee8bce3535ace53
Gaurang <gaurangjagtap@gmail.com>
2024-03-06 05:12:28 +0000

    Deployement process completed

--- obj/Release/net8.0/Airbnb.AssemblyInfo.cs
+++ obj/Release/net8.0/Airbnb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Airbnb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f593e53e66f4be8198f3bfd061969dcd74a6fb3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Airbnb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Airbnb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
